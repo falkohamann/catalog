91cc1b419d33a4fd9d671ee72d5607c554c1420c
unknown <falko.hamann@gmail.com>
2017-12-16 10:38:13 +0100

    dbfix

--- application.py
+++ application.py
@@ -24,7 +24,7 @@ CLIENT_ID = json.loads(
 APPLICATION_NAME = "Item Catalog Application"
 
 # Connect to Database and create database session
-engine = create_engine('sqlite:///./catalog/itemcatalog.db?check_same_thread=False')
+engine = create_engine('sqlite:///./catalog/itemcatalog.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
